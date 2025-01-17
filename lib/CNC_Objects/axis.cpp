c22dd9b99e46c270c7660a04df6a83159b515f49
lucasser <lucserebren@gmail.com>
2025-01-17 13:26:32 -0800

    it compiles... Heck ye!

--- lib/CNC_Objects/axis.cpp
+++ lib/CNC_Objects/axis.cpp
@@ -2,22 +2,6 @@
 #include <leveling.h>
 #include <DRV8825.h>
 
-//custom converter to export ArduinoJson elements as char
-namespace ArduinoJson {
-  template <>
-  struct Converter<char> {
-    static void toJson(char c, JsonVariant var) {
-      char buf[] = {c, 0};
-      var.set(buf);
-    }
-
-    static char fromJson(JsonVariantConst src) {
-      auto p = src.as<const char*>();
-      return p ? p[0] : 0;
-    }
-  };
-}
-
 Axis::Axis() {}
 
 Axis::Axis(JsonVariant config) {
